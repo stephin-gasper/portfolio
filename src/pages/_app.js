eab884e9627620dea44866d5a83d062e8a98efbf
Stephin Gasper <stephin.gasper21@gmail.com>
2023-12-07 20:46:10 +0530

    feat(nprogress): setup & integrate in _app.js
    #Progress bar for route changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,15 @@
 import { HeadlessApp } from "@headstartwp/next";
+import Router from "next/router";
+import NProgress from "nprogress";
 import Link from "next/link";
 
 import "../styles.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+Router.events.on("routeChangeError", () => NProgress.done());
 
 // eslint-disable-next-line react/prop-types
 const MyApp = ({ Component, pageProps }) => {
@@ -13,6 +21,20 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <HeadlessApp
       pageProps={pageProps}
+      swrConfig={{
+        /**
+         * Setting this to true will refetch content whenever the tab is refocused
+         */
+        revalidateOnFocus: false,
+        /**
+         * Settings this to true will refetch content whenever the connection is reestablished
+         */
+        revalidateOnReconnect: false,
+        /**
+         * Setting this to true will refetch content after initial load
+         */
+        revalidateOnMount: false,
+      }}
       settings={{
         // instruct the framework to use Next.js link component or your own version
         linkComponent: Link,
