e8805f9f40fe52c47da2f158fd0f5f90a36d59ef
Stephin Gasper <stephin.gasper21@gmail.com>
2023-12-11 10:16:51 +0530

    feat: import primary and secondary fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,38 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            rel="preload"
+            as="style"
+            href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Pacifico&display=swap"
+          />
+          {/* workaround added to fix onload getting removed in displayed output */}
+          <style
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+              </style>
+                <link
+                  rel="stylesheet"
+                  href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Pacifico&display=swap"
+                  media="print"
+                  onload="this.media='all';"
+                />
+              <style>`,
+            }}
+          />
+          <noscript>
+            <link
+              href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Pacifico&display=swap"
+              rel="stylesheet"
+            />
+          </noscript>
           <link rel="canonical" href="https://stephin-gasper.vercel.app/" />
         </Head>
         <body>
