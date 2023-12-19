6cf82db4d56859771fb8c213f84a0e289fb0fe5d
Stephin Gasper <stephin.gasper21@gmail.com>
2023-12-19 17:52:06 +0530

    refactor: shift styling constant for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,26 @@
 import { useState } from "react";
+import { css } from "@linaria/core";
+
+import { media } from "@/styles/breakpoints";
+
 import Logo from "./Logo";
 import Navigation from "./Navigation";
 import ThemeToggle from "./ThemeToggle";
 
-import { navStyles } from "./Header.style";
+const navStyles = css`
+  align-items: center;
+  display: grid;
+  gap: 0.625rem 1.25rem;
+  grid-template-columns: repeat(3, min-content);
+  grid-template-rows: auto auto;
+  justify-content: center;
+  margin-top: 1.25rem;
+  ${media.sm} {
+    grid-template-columns: 1fr repeat(2, min-content);
+    grid-template-rows: 1fr;
+    justify-content: start;
+  }
+`;
 
 const navigationLinks = [
   {
