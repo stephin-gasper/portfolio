db27581e3f5a81c4e8f62e8801578bdfff05595a
Stephin Gasper <stephin.gasper21@gmail.com>
2024-01-08 15:04:28 +0530

    feat: add cards for works archive page

---
+++ src/modules/works/Card/Card.style.js
@@ -0,0 +1,37 @@
+import { css } from "@linaria/core";
+
+export const cardWrapperStyles = css`
+  display: block;
+  list-style: none;
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+export const cardInnerStyles = css`
+  color: var(--color-secondary);
+  display: block;
+  text-align: center;
+  &:hover,
+  &:focus {
+    text-decoration: none;
+  }
+  .light & {
+    color: var(--color-primary);
+  }
+`;
+
+export const cardImageStyles = css`
+  width: 100%;
+`;
+
+export const cardtitleStyles = css`
+  margin: 0.5rem 0 0;
+`;
+
+export const cardDescriptionStyles = css`
+  font-size: 1em;
+  line-height: 1.5;
+  text-indent: 0;
+  margin: 0.5rem 0 0;
+`;
---
+++ src/modules/works/Card/index.js
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+import Link from "next/link";
+import { HtmlDecoder } from "@headstartwp/core/react";
+
+import {
+  cardDescriptionStyles,
+  cardImageStyles,
+  cardInnerStyles,
+  cardWrapperStyles,
+  cardtitleStyles,
+} from "./Card.style";
+
+const Card = ({ href, imgSrc, title, children }) => {
+  return (
+    <li className={cardWrapperStyles}>
+      <Link href={href} className={cardInnerStyles}>
+        <img src={imgSrc} className={cardImageStyles} alt="project" />
+        <h3 className={cardtitleStyles}>{title}</h3>
+        <p className={cardDescriptionStyles}>
+          <HtmlDecoder html={children} />
+        </p>
+      </Link>
+    </li>
+  );
+};
+
+Card.propTypes = {
+  href: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.func,
+    PropTypes.elementType,
+  ]).isRequired,
+};
+
+Card.defaultProps = {
+  imgSrc: "/placeholder-archive.png",
+};
+
+export default Card;
