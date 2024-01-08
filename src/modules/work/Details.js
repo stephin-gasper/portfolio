1f8f57fcafff3cd69a73f7ecfc1c9116ee105b8e
Stephin Gasper <stephin.gasper21@gmail.com>
2024-01-08 21:28:05 +0530

    feat: handle content, styling and structure for single work page

---
+++ src/modules/work/Details.js
@@ -0,0 +1,44 @@
+import { css } from "@linaria/core";
+import PropTypes from "prop-types";
+
+const detailsWrapperStyles = css`
+  text-indent: 0;
+`;
+
+const detailNameStyles = css`
+  background-color: var(--color-tertiary);
+  border-radius: 0.49em;
+  color: var(--color-secondary);
+  font-size: 0.7em;
+  font-weight: 600;
+  line-height: 1;
+  margin-right: 0.812em;
+  padding: 0.65em 0.812em;
+  text-transform: uppercase;
+`;
+
+const Details = ({ name, value, isLink }) =>
+  value ? (
+    <p className={detailsWrapperStyles}>
+      <small className={detailNameStyles}>{name}</small>
+      {isLink ? (
+        <a href={value} target="_blank" rel="noreferrer">
+          {value}
+        </a>
+      ) : (
+        value
+      )}
+    </p>
+  ) : null;
+
+Details.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  isLink: PropTypes.bool,
+};
+
+Details.defaultProps = {
+  isLink: false,
+};
+
+export default Details;
---
+++ src/modules/work/Work.constants.js
@@ -0,0 +1,28 @@
+export const PLATFORM_MAP = {
+  web: "Web",
+  mobile: "Mobile",
+  backend: "Backend",
+};
+
+export const TECH_STACK_MAP = {
+  javascript: "JavaScript",
+  php: "PHP",
+  wordpress: "WordPress",
+  html5: "HTML5",
+  css3: "CSS3",
+  jquery: "jQuery",
+  scss: "SCSS",
+  react: "React.js",
+  react_native: "React Native",
+  deno: "Deno",
+  node: "Node.js",
+  puppeteer: "Puppeteer",
+  next: "Next.js",
+  express: "Express.js",
+  redux: "Redux.js",
+  styled_component: "Styled Components",
+  jest: "Jest",
+  jotai: "Jotai",
+  react_testing_library: "React Testing Library",
+  react_native_testing_library: "React Native Testing Library",
+};
