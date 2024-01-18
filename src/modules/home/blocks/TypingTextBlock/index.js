94cda4d16603e7cfd9f8ed634c86f9716a8273d5
Stephin Gasper <stephin.gasper21@gmail.com>
2024-01-18 23:50:51 +0530

    feat: add TypingTextBlock
    #Use this block to display job titles

---
+++ src/modules/home/blocks/TypingTextBlock/index.js
@@ -0,0 +1,49 @@
+import PropTypes from "prop-types";
+import { cx } from "@linaria/core";
+import { defaultElement } from "@headstartwp/core/react";
+
+import useTypewriter from "./useTypeWriter";
+
+import {
+  typingTextStyles,
+  typingTextWrapperStyles,
+  blinkStyles,
+  typingTextCursorStyles,
+} from "./TypingTextBlock.style";
+
+const TypingTextBlock = ({ domNode: node = defaultElement }) => {
+  const { prefix, strings, pauseTime, typeSpeed, deleteSpeed, loop, suffix } =
+    JSON.parse(node.attribs["data-wp-block"]);
+  const { typedText, isTypingPaused, continueLoop } = useTypewriter({
+    strings,
+    pauseTime,
+    typeSpeed,
+    deleteSpeed,
+    loop,
+  });
+  return (
+    <p className={typingTextWrapperStyles}>
+      {prefix}
+      <span className={typingTextStyles}>
+        {typedText}
+        <span
+          aria-hidden="true"
+          className={cx(
+            typingTextCursorStyles,
+            isTypingPaused && blinkStyles,
+            !continueLoop && "hide",
+          )}
+        >
+          |
+        </span>
+      </span>
+      {suffix}
+    </p>
+  );
+};
+
+TypingTextBlock.propTypes = {
+  domNode: PropTypes.shape({}),
+};
+
+export default TypingTextBlock;
