cda58aff45f70c7da256961daba482f2c257cb8b
Stephin Gasper <stephin.gasper21@gmail.com>
2024-03-10 22:52:15 +0530

    feat: extract LinkButton code from ResumeDownload to create common component

---
+++ src/components/LinkButton/index.js
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+import { css } from "@linaria/core";
+
+const linkButtonStyles = css`
+  border-radius: 0.5em;
+  border: 0.125em solid var(--border-primary);
+  box-shadow: var(--border-primary) 0 0.3125em 0 0;
+  color: inherit;
+  cursor: pointer;
+  display: inline-flex;
+  padding: 0.9375em 1.5em;
+  &:focus,
+  &:hover {
+    text-decoration: none;
+    transform: scale(1.02);
+  }
+`;
+
+const LinkButton = ({ url, icon: Icon, text, ...linkRestProps }) => (
+  <a href={url} className={linkButtonStyles} {...linkRestProps}>
+    {Icon ? <Icon /> : null}
+    &nbsp; <b>{text}</b>
+  </a>
+);
+
+LinkButton.propTypes = {
+  url: PropTypes.string.isRequired,
+  icon: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.func,
+    PropTypes.elementType,
+  ]),
+  text: PropTypes.string.isRequired,
+};
+
+export default LinkButton;
