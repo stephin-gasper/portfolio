fc499982541d83133c236cf2fb987ab365927e4c
Stephin Gasper <stephin.gasper21@gmail.com>
2024-03-21 00:53:50 +0530

    feat: modify posts params for limiting fields

--- src/params.js
+++ src/params.js
@@ -6,19 +6,39 @@ export const singleParams = { postType: ["page", "post"] };
 /**
  * @type {import('@headstartwp/core').PostParams}
  */
-export const pageParams = { postType: ["page"] };
+export const pageParams = {
+  postType: "page",
+  _fields: ["content"],
+};
 
 /**
  * @type {import('@headstartwp/core').PostParams}
  */
-export const aboutPageParams = { postType: ["page"], slug: "about" };
+export const aboutPageParams = {
+  postType: "page",
+  slug: "about",
+  _fields: ["title", "content"],
+};
 
 /**
  * @type {import('@headstartwp/core').PostParams}
  */
-export const workParams = { postType: ["work"] };
+export const workParams = {
+  postType: "work",
+};
 
 /**
  * @type {import('@headstartwp/core').PostsArchiveParams}
  */
-export const worksParams = { postType: "work" };
+export const worksParams = {
+  postType: "work",
+  _fields: [
+    "id",
+    "title",
+    "slug",
+    "meta_box",
+    "excerpt",
+    "_links.wp:term",
+    "_links.wp:featuredmedia",
+  ],
+};
