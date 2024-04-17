ab83ca562532240420f20bfed841bc110b693953
Stephin Gasper <stephin.gasper21@gmail.com>
2024-04-17 18:35:30 +0530

    feat: change the file name for resume & change file path

--- src/modules/resume/Resume/Resume.constants.js
+++ src/modules/resume/Resume/Resume.constants.js
@@ -5,6 +5,6 @@ export const PDF_VIEWER_OPTIONS = {
 
 export const RESUME_PAGE_SIZE = 1;
 
-export const RESUME_PATH = "./Resume.pdf";
+export const RESUME_PATH = "./stephin-gasper-resume.pdf";
 
 export const RESIZE_OBSERVER_OPTIONS = {};
